4795380564c915942de66cc09cd937391e5a4607
James Earley <james.albert.earley@gmail.com>
2023-04-11 12:46:32 +0100

    fixed typo error with button in handlebutton click function and added functions for setOperator and appendToDisplay

--- script.js
+++ script.js
@@ -14,10 +14,20 @@ buttons.addEventListener("click", handleButtonClick);
 clearButton.addEventListener("click", clearDisplay);
 equalsButton.addEventListener("click", evaluateExpression);
 
-function handleButtonClick (event) {
-    const button = event.target;
-    console.log(button);
+function handleButtonClick(event) {
+  const button = event.target;
+  if (button.classList.contains("number")) {
+    appendToDisplay(button.getAttribute("data-number"));
+  } else if (button.classList.contains("operator")) {
+    setOperator(button.getAttribute("data-operator"));
+  }
 }
+
+function appendToDisplay(number) {
+    display.value += parseFloat(number);
+}
+
+function setOperator() {}
 
 function clearDisplay (event) {}
 
