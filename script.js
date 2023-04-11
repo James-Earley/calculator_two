389850fc9174140a8663d69a4b4b1605ce060065
James Earley <james.albert.earley@gmail.com>
2023-04-11 10:41:12 +0100

    html elements defind

---
+++ script.js
@@ -0,0 +1,5 @@
+/// grab the elements 
+const display = document.getElementById("display");
+const buttons = document.getElementById("buttons");
+const clearButton = document.getElementById("clear");
+const equalsButton = docuemnt.getElementById("equals");
