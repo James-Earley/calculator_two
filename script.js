9af5866dba8052ae2612af6b81ef23344f92ecea
James Earley <james.albert.earley@gmail.com>
2023-04-11 12:24:20 +0100

    fixed typos in element id and added first part of functionality for handleButtonClick

--- script.js
+++ script.js
@@ -2,4 +2,23 @@
 const display = document.getElementById("display");
 const buttons = document.getElementById("buttons");
 const clearButton = document.getElementById("clear");
-const equalsButton = docuemnt.getElementById("equals");
+const equalsButton = document.getElementById("equals");
+
+/// the variables 
+let currentOperator = null;
+let currentOperand = null;
+
+/// event listeners 
+
+buttons.addEventListener("click", handleButtonClick);
+clearButton.addEventListener("click", clearDisplay);
+equalsButton.addEventListener("click", evaluateExpression);
+
+function handleButtonClick (event) {
+    const button = event.target;
+    console.log(button);
+}
+
+function clearDisplay (event) {}
+
+function evaluateExpression (event) {}
