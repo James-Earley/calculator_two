07514059e8be144e3c015ac7d02e47ba5788428a
James Earley <james.albert.earley@gmail.com>
2023-04-20 11:00:13 +0100

    fixing logic to handle chain on inputs

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ const equalsButton = document.getElementById("equals");
 /// the variables 
 let firstOperator = null;
 let firstOperand = null;
-let secondOperand = null;
 let secondOperator = null;
+
 
 /// event listeners 
 
 buttons.addEventListener("click", handleButtonClick);
 clearButton.addEventListener("click", clearDisplay);
-equalsButton.addEventListener("click", evaluateExpression);
+equalsButton.addEventListener("click", operate);
 
-// event handler for button pushes
+// event handler for button pushes (add operand updaters)
 function handleButtonClick(event) {
   const button = event.target;
   if (button.classList.contains("number")) {
@@ -33,6 +33,10 @@ function appendToDisplay(number) {
 
 // sets the operator, places the present display value into firstOperand and then empties the display
 function setOperator(operator) {
+    if (firstOperator === !null) {
+    secondOperator = operator;
+    operate();  
+    }
     firstOperator = operator;
     firstOperand = display.value;
     display.value = "";
@@ -46,30 +50,31 @@ function clearDisplay (event) {
 }
 
 // does the equation 
-function evaluateExpression (event) {
-    // checks to see if all equaltion elements are present and then converts strings to numbers and to new variables
-    if (firstOperator && firstOperand && display.value) {
-        const operand1 = parseFloat(firstOperand);
-        const operand2 = parseFloat(display.value);
-        let result;
-    
-    // html operators are mapped to their corresponding operations
-     const operations = {
-       "+": (a, b) => a + b,
-       "-": (a, b) => a - b,
-       "*": (a, b) => a * b,
-       "/": (a, b) => a / b,
-     };
+function operate () {
+  // html operators are mapped to their corresponding operations
+  const operations = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+  };
 
-    //selects the correct operation by running the firstOperator against the object of operations and passed the rights values into the equation. 
-        if (firstOperator in operations) {
-        result = operations[firstOperator](operand1, operand2);
+  // checks to see if all equaltion elements are present and then converts strings to numbers and to new variables
+  if (firstOperator && firstOperand && display.value) {
+    const operand1 = parseFloat(firstOperand);
+    const operand2 = parseFloat(display.value);
+
+    let result;
+
+    //selects the correct operation by running the firstOperator against the object of operations and passed the rights values into the equation.
+    if (firstOperator in operations) {
+      result = operations[firstOperator](operand1, operand2);
     }
     // if the above operation is run, and result is now defined, the display value is now set to result and the current operator and operand results are nulled to reset them for any further user inputs
     if (result !== undefined) {
-        display.value = result;
-        firstOperator = null;
-        firstOperand = null;
+      display.value = result;
+      firstOperator = null;
+      firstOperand = null;
     }
-}
+  }
 }
